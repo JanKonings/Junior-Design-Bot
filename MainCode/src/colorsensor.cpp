49d4f80c672c6010687829be66e242f0be93b925
Andrew <lavatree05@gmail.com>
2025-10-16 13:52:19 -0400

    Updated color sensor

--- MainCode/src/colorsensor.cpp
+++ MainCode/src/colorsensor.cpp
@@ -3,7 +3,8 @@
 
 const int redLED = 2;
 const int blueLED = 3;
-const int sensorPin = A0;
+const int sensorPinL = A2;
+const int sensorPinR = A3;
 
 int ambValue = 0;
 int redValue = 0;
@@ -15,6 +16,17 @@ int xdiff = 0;
 int mag = 0;
 float rads = 0;
 float deg = 0;
+
+int ambValue2 = 0;
+int redValue2 = 0;
+int blueValue2 = 0;
+
+int ydiff2 = 0;
+int xdiff2 = 0;
+
+int mag2 = 0;
+float rads2 = 0;
+float deg2 = 0;
 
 enum Color {
   RED,
@@ -32,24 +44,28 @@ void setup() {
 
 void loop() {
   Color detected = OTHER;
+  Color detected2 = OTHER;
 
   // Red ambient light
   digitalWrite(redLED, LOW);
   digitalWrite(blueLED, LOW);
   delay(100); // allow light to stabilize
-  ambValue = analogRead(sensorPin);
+  ambValue = analogRead(sensorPinL);
+  ambValue2 = analogRead(sensorPinR);
 
   // Read red reflectance
   digitalWrite(redLED, HIGH);
   digitalWrite(blueLED, LOW);
   delay(100);
-  redValue = analogRead(sensorPin);
+  redValue = analogRead(sensorPinL);
+  redValue2 = analogRead(sensorPinR);
 
   // Read blue reflectance
   digitalWrite(redLED, LOW);
   digitalWrite(blueLED, HIGH);
   delay(100);
-  blueValue = analogRead(sensorPin);
+  blueValue = analogRead(sensorPinL);
+  blueValue2 = analogRead(sensorPinR);
 
   // Turn off both LEDs
   digitalWrite(redLED, LOW);
@@ -65,10 +81,15 @@ void loop() {
 
   ydiff = abs(blueValue - ambValue);
   xdiff = abs(redValue - ambValue);
+  ydiff2 = abs(blueValue2 - ambValue2);
+  xdiff2 = abs(redValue2 - ambValue2);
 
   mag = sqrt(sq(ydiff) + sq(xdiff));
   rads = atan2(ydiff, xdiff);
   deg = degrees(rads);
+  mag2 = sqrt(sq(ydiff2) + sq(xdiff2));
+  rads2 = atan2(ydiff2, xdiff2);
+  deg2 = degrees(rads2);
 
   // // Print cords
   // Serial.print("Decision Space: ");
@@ -76,25 +97,47 @@ void loop() {
   // Serial.print(", ");
   // Serial.println(ydiff);
 
-  if(mag < 100) {
+  if(mag < 80) {
     detected = BLACK;
-    Serial.println("BLACK ");
-  } else if (deg > 0 && deg <= 35) {
-    detected = RED;
-    Serial.println("RED ");
-  } else if (deg > 35 && deg <= 55) {
+    Serial.print("BLACK       ");
+  } else if (deg > 0 && deg <= 20) {
+    detected = RED; 
+    Serial.print("RED     ");
+  } else if (deg > 20 && deg <= 45) {
     detected = YELLOW;
-    Serial.println("YELLOW ");
-  } else if (deg > 55) {
+    Serial.print("YELLOW        ");
+  } else if (deg > 45) {
     detected = BLUE;
-    Serial.println("BLUE ");
+    Serial.print("BLUE        ");
   } else {
     detected = OTHER;
-    Serial.println("OTHER ");
+    Serial.print("OTHER       ");
+  }
+
+    if(mag2 < 80) {
+    detected2 = BLACK;
+    Serial.println("BLACK");
+  } else if (deg2 > 0 && deg2 <= 15) {
+    detected2 = RED; 
+    Serial.println("RED");
+  } else if (deg2 > 15 && deg2 <= 35) {
+    detected2 = YELLOW;
+    Serial.println("YELLOW");
+  } else if (deg2 > 35) {
+    detected2 = BLUE;
+    Serial.println("BLUE");
+  } else {
+    detected2 = OTHER;
+    Serial.println("OTHER");
   }
   
-  // Serial.print(mag);
-  // Serial.print(", ");
-  // Serial.println(deg);
+  Serial.print(mag);
+  Serial.print(", ");
+  Serial.print(deg);
+  Serial.print("      ");
+
+  Serial.print(mag2);
+  Serial.print(", ");
+  Serial.println(deg2);
 
 } 
